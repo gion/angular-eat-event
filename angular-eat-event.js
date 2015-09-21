86f56f243b9f2f4ab3997c11c15efc7bd3b6a532
gion <bogdan.gradinariu@thinslices.com>
2015-09-21 17:38:41 +0300

    rearranging some code

--- angular-eat-event.js
+++ angular-eat-event.js
@@ -8,20 +8,24 @@
 
 angular.module('eatEvent', [])
 
-  .directive('eatEvent', function() {
+  .constant('defaultEvent', 'click')
 
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs) {
-      var defaultEvent = 'click';
+  .directive('eatEvent', ['defaultEvent', function(defaultEvent) {
+    var link = function(scope, element, attrs) {
 
-        element.on(attrs.eatEvent || defaultEvent, function(event) {
+      var eventName = attrs.eatEvent || defaultEvent;
+
+      element.on(eventName, function(event) {
         event.stopPropagation();
         event.cancelBubble = true;
         event.preventDefault();
 
         return false;
       });
-    }
-  }
-});
+    };
+
+    return {
+      restrict: 'A',
+      link: link
+    };
+  }]);
